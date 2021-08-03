3c3ebf0445c6ec90b11bf48ed24944022dec32c5
jawad <jawadhalbymakary@gmail.com>
2021-08-03 05:39:44 +0300

    add firebase

---
+++ firebase.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyBLJswr_RCtn94J8NL6DJdmeOn0EJHCZUw",
+    authDomain: "clone2-2c9c1.firebaseapp.com",
+    projectId: "clone2-2c9c1",
+    storageBucket: "clone2-2c9c1.appspot.com",
+    messagingSenderId: "722711691738",
+    appId: "1:722711691738:web:82de81e9812ddc89ba80e3",
+    measurementId: "G-9HM35W451M"
+  };
+  firebaseConfig.initializeApp(firebaseConfig)
+  firebaseConfig.analytics()
