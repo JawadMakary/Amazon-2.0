1542cde9d97a4511035e526665f452aea8544d9c
jawad <jawadhalbymakary@gmail.com>
2021-08-04 06:16:29 +0300

    total fixed and numeral library added

--- cart.js
+++ cart.js
@@ -1,67 +1,62 @@
-const getItemsCart=()=>{
-    
-db.collection("cart-items").onSnapshot((snapshot) => {
-  let cartItems=[]
-    snapshot.docs.forEach((doc)=>{
-        cartItems.push({
-            id:doc.id,
-            ...doc.data()
+function getCartItems() {
+    db.collection("cart-items").onSnapshot((snapshot) => {
+        let cartItems = [];
+        snapshot.docs.forEach((doc) => {
+            cartItems.push({
+                id: doc.id, 
+                ...doc.data()
+            })
         })
+        generateCartItems(cartItems);
+        getTotalCost(cartItems)
     })
-    generateCartItems(cartItems)
-    getTotalCost(cartItems)
-})
-
 }
-const getTotalCost=(items)=>{
-    let totalCost=0;
+
+function getTotalCost(items){
+    let totalCost = 0;
     items.forEach((item)=>{
         totalCost += (item.price * item.quantity);
     })
-    document.querySelector(".total-cost-number").innerText=
-    ` $ ${totalCost}`
-    
-
+    document.querySelector(".total-cost-number").innerText = numeral(totalCost).format('$0,0.00');
 }
 
- const decreaseCount=(itemId)=>{
-     let cartItem=db.collection("cart-items").doc(itemId)
-     cartItem.get().then(function(doc){
-         if(doc.exists){
-             if(doc.data().quantity>1){
+function decreaseCount(itemId) {
+    let cartItem = db.collection("cart-items").doc(itemId);
+    cartItem.get().then(function(doc) {
+        if (doc.exists) {
+            if (doc.data().quantity > 1) {
                 cartItem.update({
-                    quantity:doc.data().quantity-1 
+                    quantity: doc.data().quantity - 1
                 })
-             }
-             else{
-                 deleteItem(itemId)
-             }
-           
-         }
-     })
- }
-
- const increaseCount=(itemId)=>{
-    let cartItem=db.collection("cart-items").doc(itemId)
-    cartItem.get().then(function(doc){
-        if(doc.exists){
-            if(doc.data().quantity>0){
-               cartItem.update({
-                   quantity:doc.data().quantity+1 
-               })
             }
-          
+            else{
+                deleteItem(itemId)
+            }
         }
     })
 }
-const deleteItem=(itemId)=>{
-    db.collection("cart-items").doc(itemId).delete();
-} 
 
- const generateCartItems=(cartItems)=>{
-let itemsHTML='';
-cartItems.forEach((item)=>{
-    itemsHTML += `
+function increaseCount(itemId){
+    let cartItem = db.collection("cart-items").doc(itemId);
+    cartItem.get().then(function(doc) {
+        if (doc.exists) {
+            if (doc.data().quantity > 0) {
+                cartItem.update({
+                    quantity: doc.data().quantity + 1
+                })
+            }
+        }
+    })
+}
+
+function deleteItem(itemId){
+    db.collection("cart-items").doc(itemId).delete();
+}
+
+function generateCartItems(cartItems) {
+    let itemsHTML = "";
+    cartItems.forEach((item) => {
+        itemsHTML += `
             <div class="cart-item flex items-center pb-4 border-b border-gray-100">
                 <div class="cart-item-image w-40 h-24 bg-white p-4 rounded-lg">
                     <img class="w-full h-full object-contain" src="${item.image}">
@@ -70,7 +65,7 @@ cartItems.forEach((item)=>{
                     <div class="cart-item-title font-bold text-sm text-gray-600">
                     ${item.name}
                     </div>
-                  
+                   
                 </div>
                 <div class="cart-item-counter w-48 flex items-center">
                     <div data-id="${item.id}" class="cart-item-decrease text-gray-400 bg-gray-100 rounded h-6 w-6 flex justify-center items-center hover:bg-gray-200 mr-2 cursor-pointer">
@@ -82,43 +77,43 @@ cartItems.forEach((item)=>{
                     </div>
                 </div>
                 <div class="cart-item-total-cost w-48 font-bold text-gray-400">
-                    ${item.price * item.quantity}
+                    ${numeral(item.price * item.quantity).format('$0,0.00')}
                 </div>
-                <div  data-id="${item.id}"  class="cart-item-delete w-10 font-bold text-gray-300 cursor-pointer hover:text-gray-400">
+                <div data-id="${item.id}"  class="cart-item-delete w-10 font-bold text-gray-300 cursor-pointer hover:text-gray-400">
                     <i class="fas fa-times"></i>
                 </div>
             </div>
-    
-    
-    `
-
-})
-document.querySelector(".cart-items").innerHTML=itemsHTML
-createEventListeners()
-}
-const createEventListeners=()=>{
-let decreaseButtons=document.querySelectorAll(".cart-item-decrease")
-let increaseButtons=document.querySelectorAll(".cart-item-increase")
-let deleteButtons=document.querySelectorAll(".cart-item-delete")
-deleteButtons.forEach((btn_del)=>{
-    btn_del.addEventListener("click",function(){
-        deleteItem(btn_del.dataset.id)
+        `
     })
-})
-decreaseButtons.forEach((button)=>{
-    button.addEventListener("click",function(){
-       decreaseCount(button.dataset.id)
-    })
-    
-})
-increaseButtons.forEach((btn)=>{
-   btn.addEventListener("click",function(){
-       increaseCount(btn.dataset.id)
-     })
-})
+    document.querySelector(".cart-items").innerHTML = itemsHTML;
+    createEventListeners();
 }
 
+function createEventListeners() {
+    let decreaseButtons = document.querySelectorAll(".cart-item-decrease");
+    let increaseButtons = document.querySelectorAll(".cart-item-increase");
+
+    let deleteButtons = document.querySelectorAll(".cart-item-delete");
+
+    decreaseButtons.forEach((button) => {
+        button.addEventListener("click", function(){
+            decreaseCount(button.dataset.id);
+        })
+    })
+
+    increaseButtons.forEach((button) => {
+        button.addEventListener("click", function() {
+            increaseCount(button.dataset.id)
+        })
+    })
+
+    deleteButtons.forEach((button)=>{
+        button.addEventListener("click", function(){
+            deleteItem(button.dataset.id)
+        })
+    })
+
+}
 
 
-
-getItemsCart()
+getCartItems();
