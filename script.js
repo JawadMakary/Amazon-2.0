28b3f51c6a492ba105a1feb37ecddc9848901eaf
jawad <jawadhalbymakary@gmail.com>
2021-08-04 03:11:33 +0300

    add to cart functionality added

--- script.js
+++ script.js
@@ -16,36 +16,65 @@ const getItems=()=>{
     });
 
 }
+const addToCart=(item)=>{
+   
+ let cartItem=  db.collection("cart-items").doc(item.id)
+ cartItem.get().then(function(doc){
+     if(doc.exists){
+         cartItem.update({
+             quantity:doc.data().quantity+1
+         })
+     }
+     else{
+        cartItem.set({
+            image:item.image,
+            name:item.name,
+            price:item.price,
+            quantity:1
+        })
+     }
 
+ })
+ 
+ 
+}
 const generateItems=(items)=>{
     let itemsHTML=""
     items.forEach((item)=>{
-        itemsHTML+=`
-        <div class="main-product mr-5 ">
+        let doc=document.createElement("div")
+        doc.classList.add("main-product","mr-5")
+        doc.innerHTML=`
         <div class="product-image w-48 h-52 bg-white rounded-lg p-4">
-             <img class="w-full h-full object-contain" src="${item.image}" alt="">
-        </div>
-        <div class="product-name text-gray-700
-        font-bold mt-2 text-sm
-        ">
-            ${item.name}
-        </div>
-        
-       
-      
-        <div class="product-price font-bold text-gray-700
-        text-lg">
-           $  ${item.price} 
-        </div>
-        <div class="add-to-cart h-8 w-28 bg-yellow-500 flex items-center justify-center cursor-pointer text-white
-        hover:bg-yellow-600 rounded text-md">
-             Add  to cart
-        </div>
-    </div>      
+        <img class="w-full h-full object-contain" src="${item.image}" alt="">
+   </div>
+   <div class="product-name text-gray-700
+   font-bold mt-2 text-sm
+   ">
+       ${item.name}
+   </div>
+   
+  
+ 
+   <div class="product-price font-bold text-gray-700
+   text-lg">
+      $  ${item.price} 
+   </div>
+     
         `
+     let addToCartEl=document.createElement("div")
+     addToCartEl.classList.add("hover:bg-yellow-600", "cursor-pointer", "product-add", "h-8", "w-28", "rounded", "bg-yellow-500", "text-white", "text-md", "flex", "justify-center", "items-center");
+  
+        
+     addToCartEl.innerText="Add To Cart"
+     addToCartEl.addEventListener("click",function(){
+         addToCart(item)
+     })
+        doc.appendChild(addToCartEl)
+        document.querySelector(".main-section-products").
+        appendChild(doc)
+
     })
-    document.querySelector(".main-section-products").
-    innerHTML=itemsHTML
+    
     
     
 }
